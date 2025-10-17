a95739ec400519cb6619dc6080bb238f94e3d72b
Andrés Leiva Hernández <89427059+AndresLeiva93@users.noreply.github.com>
2025-10-17 12:28:08 -0300

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    // Estas dependencias deben estar listadas en package.json (dev-dependencies)
+    // para que Tailwind CSS funcione correctamente en el proceso de build de Vite.
+    'tailwindcss': {},
+    'autoprefixer': {},
+  },
+};
